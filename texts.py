5540e65b9ed3611e7a6dfa8686a7b693de520a49
Bonart <zzz@yyy.xxx>
2019-07-28 12:35:29 +0300

    descriptions

---
+++ texts.py
@@ -0,0 +1,49 @@
+DISCRIPT = f"Welcome, I'm a bot-ophthalmologist, you can upload a " \
+           "snapshot of the retinal fundus, and i will make a prediction the " \
+           "presence of diabetic retinopathy in the picture on a scale of 0 to 4."
+
+
+ABOUT_SERVICE = 'Imagine that you can detect blindness before it happened. ' \
+        'Millions of people suffer from diabetic retinopathy, the' \
+        'leading cause of blindness among working older people. \n \n' \
+        'This project was created to help ophthalmologists identify and prevent' \
+        'this disease among people living in rural areas where it is difficult to ' \
+        'conduct a medical examination. \n \nA doctor can upload a retinal image to this bot, ' \
+        'which diagnoses diabetic retinopathy from 0 to 4, which corresponds to"No DR", "Mild", "Moderate", ' \
+        '"Severe", "Proliferative DR", and he will also provide a picture with a heat map of ROI.'
+
+ABOUT_PREDICTION_MODEL = 'The algorithm that makes predictions is a convolutional ' \
+                         'neural network with a ResNet18 architecture. \n \n'\
+                         'This CNN was trained on data sets like APTOS 2019 Blind Detection and ' \
+                         'on 20,000 images from the Open EyePACS data set. \n' \
+                         'This CNN has roc-auc 0.78% \nFor more info: https://github.com/OldBonhart'
+
+
+OUT_ETHOS = 'Here, in EyeMedIndustries, we specialize ' \
+            'in developing and producing advanced ' \
+            'technologies for predicting the pathogenesis ' \
+            'of the human eye, and we strive to highlight, ' \
+            'working with clients, the creation of progressive, ' \
+            'forward-looking, non-standard add-ons that ' \
+            'fit specific tasks and needs. We are proud to support ' \
+            'our creative independence in a rapidly declining  ' \
+            'and highly competitive market.'
+
+
+FILE_DISCRIPT = 'This is not an image, please upload a retina image as a photo, not as a file'
+
+HELP_DISCRIPT = 'For diagnosis and picture with ROI, upload a snapshot of the retinal fundus'
+
+LIST_DISCRIPT = "Application of artificial intelligence in ophthalmology \n" \
+                "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6133903/ \n" \
+                "About ResNet architecture \n" \
+                "https://arxiv.org/abs/1512.03385 \n" \
+                "Diagnostic Error in Medicine \n" \
+                "https://www.degruyter.com/view/j/dx.2018.5.issue-4/dx-2018-0095/dx-2018-0095.xml \n" \
+                "More details about this bot \n" \
+                "https://github.com/"
+
+CONTACT_DISCRIPT = "telegram - @Loon12 \n" \
+                   "github - https://github.com/OldBonhart"
+
+# med eye industries
